2d04c6fce051271bf7d598f5e65f9872613ec8a1
Christian Kubel Højmose <hjmose@gmail.com>
2021-07-23 17:53:23 +0200

    Assigment 10.2

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('Hello world');
   return (
     <View style={styles.container}>
       <Text>This is my first app</Text>
